94dd51b5cdd65c1756845926901d8bf9488ffa10
Armen <front2@greca.co>
2024-10-03 13:58:27 +0400

    fix: image priority

--- app/(BaseLayout)/about-us/_components/FirstSection/HeroImage.tsx
+++ app/(BaseLayout)/about-us/_components/FirstSection/HeroImage.tsx
@@ -8,7 +8,7 @@ const HeroImage = () => {
       duration={0.5}
       className="relative md:absolute md:right-[-20%] md:top-10 lg:top-[unset] lg:right-[unset] lg:relative w-[296px] h-[364px] md:w-[465px] md:h-[572px] xl:w-[566px] xl:h-[696px] 2xl:w-[733px] 2xl:h-[902px] flex-shrink-0 mt-0  lg:-mt-16 -2xl:-mt-20"
     >
-      <Image src="/images/head.png" alt="hero" layout="fill" />
+      <Image src="/images/head.png" alt="hero" layout="fill" priority />
     </Transition>
   );
 };
